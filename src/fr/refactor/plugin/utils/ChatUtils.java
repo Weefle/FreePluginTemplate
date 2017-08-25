061c79522a59be30e01ec90f1db7b9d4bfccac0b
MinusKube <minuskube@gmail.com>
2017-08-25 22:39:02 +0200

    Clean everything, fix typos, fix problems, add .gitignore

--- src/fr/refactor/plugin/utils/ChatUtils.java
+++ src/fr/refactor/plugin/utils/ChatUtils.java
@@ -2,6 +2,6 @@ package fr.refactor.plugin.utils;
 
 public class ChatUtils {
 
-   public static String version = "0.0.1";
-   public static String name = "FreePluginTemplate";
+   public static final String VERSION = "0.0.1";
+   public static final String NAME = "FreePluginTemplate";
 }
